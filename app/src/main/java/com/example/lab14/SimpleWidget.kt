068b7be1d394d1d2d3f74d683b66fd6dae2a8e96
Thomas <chuchuv3l0z@gmail.com>
2025-11-17 22:29:27 -0500

    feat: implementar SimpleWidget y contenido básico del widget

--- app/src/main/java/com/example/lab14/SimpleWidget.kt
+++ app/src/main/java/com/example/lab14/SimpleWidget.kt
@@ -1,4 +1,9 @@
 package com.example.lab14
 
-class SimpleWidget {
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
 }
+
